877392346b2970f651059c927cc8bcf78db5c28c
Nico <nmc0011@alu.medac.es>
2025-09-25 19:55:26 +0200

    prueba git

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Nexa',
-      home: Text('Hola nexa!'),
+      home: Text('Hola nexa'),
     );
   }
 }
